29f971f660b7394f1a39f36f18fb06dbcc926307
Enrique Díaz <endiba360@gmail.com>
2019-11-11 12:08:04 -0600

    lexer file created
    Here we define our tokens using LexerGenerator class from rply to create our lexer

---
+++ lexer.py
@@ -0,0 +1,25 @@
+from rply import LexerGenerator
+
+class Lexer()
+    def __init__(self):
+        self.lexer = LexerGenerator()
+
+    def __add__tokens(self):
+        # Print
+        self.lexer.add('PRINT', r'print')
+        # Parenthesis
+        self.lexer.add('OPEN_PAREN', r'\(')
+        self.lexer.add('CLOSE_PAREN', r'\)')
+        # Semi Colon
+        self.lexer.add('SEMI_COLON', r'\;')
+        # Operators
+        self.lexer.add('SUM', r'\+')
+        self.lexer.add('SUB', r'\-')
+        # Number
+        self.lexer.add('NUMBER', r'\d+')
+        # Ignore spaces
+        self.lexer.ignore('\s+')
+
+    def get_lexer(self):
+        self.__add__tokens()
+        return self.lexer.build()
