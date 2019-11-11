566b1d6d7fac445baaff7662baa47cb0cb2284b9
Enrique Díaz <endiba360@gmail.com>
2019-11-11 12:59:02 -0600

    parser file created
    using ParserGenerator from rply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lexer import Lexer
+from parser import Parser
 
 text_input = """
 print(4 + 4 - 2);
@@ -7,5 +8,7 @@ print(4 + 4 - 2);
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
 
-for token in tokens:
-    print(token)
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
---
+++ parser.py
@@ -0,0 +1,38 @@
+from rply import ParserGenerator
+from ast import Number, Sum, Sub, Print
+
+
+class Parser:
+    def __init__(self):
+        self.pg = ParserGenerator(
+            # A list of all token names accepted by the parser.
+            ['NUMBER', 'PRINT', 'OPEN_PAREN', 'CLOSE_PAREN',
+             'SEMI_COLON', 'SUM', 'SUB']
+        )
+
+    def parse(self):
+        @self.pg.production('program : PRINT OPEN_PAREN expression CLOSE_PAREN SEMI_COLON')
+        def program(p):
+            return Print(p[2])
+
+        @self.pg.production('expression : expression SUM expression')
+        @self.pg.production('expression : expression SUB expression')
+        def expression(p):
+            left = p[0]
+            right = p[2]
+            operator = p[1]
+            if operator.gettokentype() == 'SUM':
+                return Sum(left, right)
+            elif operator.gettokentype() == 'SUB':
+                return Sub(left, right)
+
+        @self.pg.production('expression : NUMBER')
+        def number(p):
+            return Number(p[0].value)
+
+        @self.pg.error
+        def error_handle(token):
+            raise ValueError(token)
+
+    def get_parser(self):
+        return self.pg.build()
